fe7726d9987786b97d7a704d7fe3a30bdaf39cfe
Tabitha Chang <changt6@bxscience.edu>
2020-06-08 11:12:41 -0400

    Global Variables Script
    To set properly, go to Project -> Project Settings -> Autoload and add it
    
    Used to maintain position of the player across scenes and will likely be needed to keep track of order requests and such in the future
    To maintain player position, add the following to the player script:
    func _init():
    	load("res://Scripts/Global.gd")
    	self.set_position(Global.pos)

---
+++ Scripts/Global.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var pos = Vector2(160, 96)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
