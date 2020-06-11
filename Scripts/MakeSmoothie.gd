308b67f860f0fdb6e7b7ab38005be2cba64b5877
Tabitha Chang <changt6@bxscience.edu>
2020-06-11 17:25:54 -0400

    Make Smoothie

---
+++ Scripts/MakeSmoothie.gd
@@ -0,0 +1,80 @@
+extends Node
+
+
+#make sure the items are stacking
+var height = -375
+var change = 15
+
+#keep track of how much we've got of each (probably good for ordering)
+var numBread = 0
+var numCheese =0
+var numHam =0
+var numLettuce=0
+var numOnion=0
+var numTomato=0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#on_blank_pressed methods will add the related object to the sandwich
+func _on_Bread_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/bread.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+
+	self.add_child(addon)
+	height-=change 
+	numBread+=1
+
+
+func _on_Cheese_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/cheesy.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+	self.add_child(addon)
+	height-=change 
+	numCheese+=1
+
+
+func _on_Ham_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/Ham.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+	self.add_child(addon)
+	height-=change 
+	numHam+=1
+
+
+func _on_Lettuce_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/lettuce.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+	self.add_child(addon)
+	height-=change 
+	numLettuce+=1
+
+
+func _on_Onion_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/onion.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+	self.add_child(addon)
+	height-=change
+	numOnion +=1
+
+
+func _on_Tomato_pressed():
+	var addon = Sprite.new() 
+	addon.texture = load("res://Assets/tomato.png")
+	addon.set_scale(Vector2(0.25, 0.25))
+	addon.set_offset(Vector2(2100, 460*4+height))
+	self.add_child(addon)
+	height-=change
+	numTomato +=1
