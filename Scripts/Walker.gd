141823f8ccc6174d19c40efc25942252d77c0d20
Tabitha Chang <changt6@bxscience.edu>
2020-06-27 13:04:25 -0400

    Update Walker.gd
    walker can now hide

--- Scripts/Walker.gd
+++ Scripts/Walker.gd
@@ -1,42 +1,34 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
 
 var velocity = Vector2()
 var rotation_dir =0
 
+func _init():
+	load("res://Scripts/Global.gd")
+	self.set_position(Global.pos)
+
 func get_input():
 	velocity = Vector2()
 	rotation_dir=0
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		set_rotation_degrees(-90)
+		Global.isHiding= false
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		set_rotation_degrees(90)
+		Global.isHiding= false
+
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
-		set_rotation_degrees(180)
+		Global.isHiding= false
+
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-		set_rotation_degrees(0)
-	if Input.is_key_pressed(KEY_A):
-		velocity.x-=1
-		set_rotation_degrees(90)
-	if Input.is_key_pressed(KEY_D):
-		velocity.x+=1
-		set_rotation_degrees(-90)
-	if Input.is_key_pressed(KEY_W):
-		velocity.y-=1
-		set_rotation_degrees(180)
-	if Input.is_key_pressed(KEY_S):
-		velocity.y+=1
-		set_rotation_degrees(0)
+		Global.isHiding= false
 
 
 
@@ -46,5 +38,9 @@ func _physics_process(delta):
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity)
+	if(Global.isHiding):
+		self.visible = false
+	else:
+		self.visible = true
 
 
